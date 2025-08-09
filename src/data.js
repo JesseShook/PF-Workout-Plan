d3ad02a3314d403ac97517553f7efc8fd6b94936
JesseShook <jessedshook@gmail.com>
2025-08-09 11:15:46 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const defaultRoutine = [
+  {
+    day: 'Monday',
+    exercises: [
+      { name: 'Bench Press', goal: '8-12 reps', sets: [] },
+      { name: 'Squats', goal: '6-10 reps', sets: [] },
+    ],
+  },
+  {
+    day: 'Tuesday',
+    exercises: [
+      { name: 'Deadlifts', goal: '5-8 reps', sets: [] },
+      { name: 'Pull-Ups', goal: '8-12 reps', sets: [] },
+    ],
+  },
+];
